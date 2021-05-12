3785f5b7bc15e3bbcd18fe8561860a515e1dfb49
Ojas Saxena <ojasva18352@iiitd.ac.in>
2021-05-12 19:02:51 +0530

    added db

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:id>" , views.index , name="index"),
+    path("lists/<int:id>" , views.index , name="index"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from typing import Text
 from django.shortcuts import render 
 from django.http import HttpResponse
 from .models import Item , ToDoList
@@ -5,4 +6,8 @@ from .models import Item , ToDoList
 
 def index(response , id):
     ls = ToDoList.objects.get(id=id)
-    return HttpResponse("<h1> %s </h1>" %ls.name)
+    try:
+        items = ls.item_set.get(id=1)
+    except:
+        items = Item(text="No Items in List yet" , complete=False)
+    return HttpResponse("<h1> %s </h1> <br> %s " %(ls.name , items.text))
