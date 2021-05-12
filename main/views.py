40c11de7605460b18f61adcbeca4260e936dc675
Ojas Saxena <ojasva18352@iiitd.ac.in>
2021-05-12 12:22:55 +0530

    update

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render 
+from django.http import HttpResponse
 # Create your views here.
+
+def index(response):
+    return HttpResponse("<h1> OJASVA SAXENA </h1>")
+
+def view1(response):
+    return HttpResponse("<h2> NEW VIEW </h2>")
