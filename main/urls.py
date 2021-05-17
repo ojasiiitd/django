e0bea948763942f6afdb653810e7701529229cd3
Ojas Saxena <ojasva18352@iiitd.ac.in>
2021-05-17 15:03:32 +0530

    new user regs

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("lists/<int:id>" , views.index , name="index"),
     path("" , views.home , name="home"),
-    path("create/" , views.create , name="create")
+    path("create/" , views.create , name="create"),
+    
 ]
